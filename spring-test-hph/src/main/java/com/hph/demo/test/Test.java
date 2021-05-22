dd164affe82940ac5ba4646326131b38df9105f7
hupenghui <1049970895@qq.com>
2021-05-22 12:39:34 +0800

    更新

--- spring-test-hph/src/main/java/com/hph/demo/test/Test.java
+++ spring-test-hph/src/main/java/com/hph/demo/test/Test.java
@@ -6,19 +6,41 @@ package com.hph.demo.test;
  */
 public class Test implements Runnable{
 
-	private final boolean t = false;
+	private boolean t = false;
 
-	private Object o1 = new Object();
+	private final Object o1 = new Object();
+
+	private final Object o2 = new Object();
 
 	@Override
 	public void run() {
 
-		if(!t) {
-			while(true) {
+		ThreadLocal
 
+		if(!t) {
+			t = true;
+			synchronized (o1) {
+				try {
+					Thread.sleep(3000);
+					synchronized (o2) {
+						System.out.println("1111111111");
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}else {
+			synchronized (o2) {
+				try {
+					Thread.sleep(3000);
+					synchronized (o1) {
+						System.out.println("22222222222");
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
 		}
-		System.out.println("11111111111111");
 	}
 
 	public static void main(String[] args) {
@@ -26,6 +48,7 @@ public class Test implements Runnable{
 		Test test = new Test();
 		Thread thread1 = new Thread(test);
 		Thread thread2 = new Thread(test);
-
+		thread1.start();
+		thread2.start();
 	}
 }
